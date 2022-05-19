012c1696c61251b53f7a49b2da76a5589f3b74b0
swssl <81415137+swssl@users.noreply.github.com>
2022-05-19 20:56:47 +0200

    Neue Struktur wegen CircularImportError

---
+++ hikerz/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from hikerz.db import *
+from .views import views
+
+
+app = Flask(__name__)
+app.secret_key = b'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(views)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def user_loader(username):
+    return User.query.get(username)
