883d6d43d82755fc37f207239d9274fdab4920ec
lfast123 <louis.fast@fh-bielefeld.de>
2022-05-20 18:07:39 +0200

    template alleTouren als view hinzugefügt

---
+++ hikerz/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, redirect, request
+from .db import User
+from .forms import *
+
+# Hier stehen die URL-Endpunkte/Routes
+
+views = Blueprint("views", __name__, template_folder="templates")
+
+@views.route('/hello')
+def index():
+    test_user = User("admin", "admin@localhost.org", "admin", 0)
+    return render_template('base.html', test_user = test_user)
+
+@views.route('/login')
+def login():
+    test_user = User("admin", "admin@localhost.org", "admin", 0)
+    return render_template('login.html', test_user=test_user) 
+
+@views.route('/routeoverview')
+def routeOverview():
+    return render_template('alleTouren.html')
