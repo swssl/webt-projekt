f1d5b4adfaa569c5d1530cdf6670f68d1c5071d1
swssl <81415137+swssl@users.noreply.github.com>
2022-05-22 21:46:31 +0200

    Added login and registration to views.py, forms.py and database. Added corresponding html-files.

---
+++ hikerz/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, EqualTo, Email, InputRequired
+from .db import User
+
+
+class LoginForm(FlaskForm):
+    user_name = StringField('Benutzername', validators=[InputRequired()])
+    password = PasswordField('Passwort', validators=[InputRequired()])
+    submit = SubmitField('Anmelden')
+
+    def validate(self):     #Extends the validate-function
+        if not super(LoginForm, self).validate():
+            return False
+        user = User.query.filter_by(username=self.user_name.data).first()
+        if not user:     # tests if username exists
+            self.user_name.errors.append("Ungültige Login-Daten")
+            return False
+        if user.password != self.password.data:     #Tests if password is correct
+            self.password.errors.append('Ungültige Login-Daten')
+            return False
+        return True
+
+
+class RegistrationForm(FlaskForm):
+    user_name = StringField('Benutzername', validators=[InputRequired()])
+    email = StringField(
+        'E-Mail Addresse', validators=[Email("Bitte überprüfe die E-Mail-Adresse")])
+    password = PasswordField('Passwort', [
+        DataRequired(),
+        EqualTo('confirm_password', message='Die Passwörter stimmen nicht überein')
+    ])
+    confirm_password = PasswordField('Passwort', [InputRequired()])
+    captcha = BooleanField('Ich bin kein Roboter', [InputRequired()])
+    submit = SubmitField("Konto erstellen")
+
+    def validate(self):
+        if not super(RegistrationForm, self).validate():
+            return False
+        user = User.query.filter_by(username=self.user_name.data).first()
+        if user:        #Tests, if the username is already assigned
+            self.user_name.errors.append("Der Benutzername ist bereits vergeben")
+            return False
+        return True
+        
--- hikerz/views.py
+++ hikerz/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, request
-from .db import User
+from flask_login import current_user, login_required, login_user, logout_user
+from .db import *
 from .forms import *
 
 # Hier stehen die URL-Endpunkte/Routes
@@ -8,10 +9,33 @@ views = Blueprint("views", __name__, template_folder="templates")
 
 @views.route('/hello')
 def index():
-    test_user = User("admin", "admin@localhost.org", "admin", 0)
-    return render_template('base.html', test_user = test_user)
+    return render_template('base.html')
 
-@views.route('/login')
+@views.route('/login', methods=['GET', 'POST'])
 def login():
-    test_user = User("admin", "admin@localhost.org", "admin", 0)
-    return render_template('login.html', test_user=test_user)
+    if current_user.is_authenticated:  # redirect to home if user is already logged in
+        return redirect("hello")
+    form = LoginForm()
+    if form.validate_on_submit():  # if valid data ist send by POST:
+        user = User.query.get(form.data['user_name'])  # query user from db
+        login_user(user)
+        return redirect("hello")
+    return render_template('login.html', login_form=form)
+
+@views.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(form.data['user_name'],
+                        form.data["email"], form.data["password"], 1) # create new user
+        db.session.add(new_user)
+        db.session.commit()     # write new user to db
+        return redirect("login")
+    return render_template('register.html', registration_form=form)
+
+@views.route('/logout')
+@login_required
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+    return redirect("login")
