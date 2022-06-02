ddd36a31adba3fca3f11b21f5b43ab67ccbea936
swssl <81415137+swssl@users.noreply.github.com>
2022-06-02 22:23:37 +0200

    Added homepage for non-logged in access

--- hikerz/views.py
+++ hikerz/views.py
@@ -8,19 +8,25 @@ from .forms import *
 
 views = Blueprint("views", __name__, template_folder="templates")
 
-@views.route('/hello')
+@views.route('/')
 def index():
-    return render_template('base.html')
+    if current_user.is_authenticated: return redirect('home')
+    else: return render_template("index.html")
+
+@views.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
 
 @views.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:  # redirect to home if user is already logged in
-        return redirect("hello")
+        return redirect("home")
     form = LoginForm()
     if form.validate_on_submit():  # if valid data ist send by POST:
         user = User.query.get(form.data['user_name'])  # query user from db
         login_user(user)
-        return redirect("hello")
+        return redirect("home")
     return render_template('login.html', login_form=form)
 
 @views.route('/register', methods=["GET", "POST"])
@@ -28,7 +34,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         new_user = User(form.data['user_name'],
-                        form.data["email"], form.data["password"], 1) # create new user
+                        form.data["email"], form.data["password"], 0) # create new user
         db.session.add(new_user)
         db.session.commit()     # write new user to db
         return redirect("login")
@@ -40,7 +46,7 @@ def logout():
     if current_user.is_authenticated:
         logout_user()
         return redirect("login")
-    return render_template('login.html', test_user=test_user) 
+    return render_template('login.html') 
 
 
 @views.route('/routeoverview')
